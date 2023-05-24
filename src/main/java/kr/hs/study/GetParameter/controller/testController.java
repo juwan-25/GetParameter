25a13cff256c84df6f76bd326f39da312bd858e0
Juwan Lee <ljw05w5@gmail.com>
2023-05-24 12:14:04 +0900

    PathVariable, RequestParam : 데이터 가져와서 출력하기

--- src/main/java/kr/hs/study/GetParameter/controller/testController.java
+++ src/main/java/kr/hs/study/GetParameter/controller/testController.java
@@ -2,6 +2,8 @@ package kr.hs.study.GetParameter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +36,23 @@ public class testController {
         String a = req.getParameter("data1");
         String b = req.getParameter("data2");
         String[] c = req.getParameterValues("data3");
+        System.out.print(a+" "+b+" ");
+        for(String i : c) System.out.print(i+" ");
+
+        return "result";
+    }
+
+    @GetMapping("/test4/{a}/{b}/{c}")
+    public String test3(@PathVariable String a,@PathVariable String b,@PathVariable String c){
+        System.out.println(a+" "+b+" "+c);
+
+        return "result";
+    }
+
+    @GetMapping("/test5")
+    public String test4(@RequestParam("data1") String a,
+                        @RequestParam("data2") String b,
+                        @RequestParam("data3") String [] c){
         System.out.print(a+" "+b+" ");
         for(String i : c) System.out.print(i+" ");
 
